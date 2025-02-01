032020e483a789d1e51420c8455beac293d287bc
Mohamed Atif <mohamedabotreka40@gmail.com>
2025-02-01 03:08:45 +0200

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
 //To allow our app to use json
 app.use(express.json());
@@ -21,3 +22,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
