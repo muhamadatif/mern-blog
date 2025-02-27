a8fff1c446d90696ef85db05eb0af8c371429bec
Mohamed Atif <mohamedabotreka40@gmail.com>
2025-02-27 22:06:44 +0200

    add signout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,14 +13,14 @@ const App = () => {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
         </Route>
-        <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComp />
     </BrowserRouter>
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,6 +3,8 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
   const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
+
   return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
