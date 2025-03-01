7447073e7a0afadb4f2c639ed1ae2fc1e64406a4
Mohamed Atif <mohamedabotreka40@gmail.com>
2025-03-01 03:38:33 +0200

    add show more functionality to the post results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,19 +5,42 @@ import { Link } from "react-router-dom";
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await fetch(`/api/post/getPosts?userId=${currentUser._id}`);
         const data = await res.json();
-        if (res.ok) setUserPosts(data.posts);
+        if (res.ok) {
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
+        }
       } catch (error) {
         console.log(error);
       }
     };
     if (currentUser.isAdmin) fetchPosts();
   }, [currentUser._id, currentUser.isAdmin]);
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`,
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="dark: table-auto overflow-x-scroll p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 md:mx-auto">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -74,6 +97,14 @@ const DashPosts = () => {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full self-center py-7 text-sm text-teal-500"
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have not posts yet</p>
