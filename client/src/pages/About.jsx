8ed527afe90d6084a4c6eecbb0dbe0ac6ad15c4d
Mohamed Atif <mohamedabotreka40@gmail.com>
2025-03-06 14:11:13 +0200

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,38 @@
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="mx-auto max-w-2xl p-3 text-center">
+        <div>
+          <h1 className="my-7 text-center text-3xl font-semibold">
+            About Mohamed&apos;s Blog
+          </h1>
+          <div className="text-md flex flex-col gap-6 text-gray-500">
+            <p>
+              Welcome to Mohamed&apos;s Blog! This blog was created by Mohamed
+              Atef as a paersonal project to share his thoughts and ideas with
+              the world. Mohamed is a passionate software engineer who loves to
+              write about technology, programming, and software development. He
+              hopes that you enjoy reading his blog posts as much as he enjoys
+              writing them.
+            </p>
+            <p>
+              On this blog, you will find a variety of articles on different
+              topics such as JavaScript, React.js, Next.js, and more. Mohamed is
+              always learning new things and he loves to share his knowledge
+              with others. If you have any questions or feedback, feel free to
+              reach out to him on social media or via email.
+            </p>
+            <p>
+              We encourage you to to leave comments on our posts and engage with
+              other readers, You can like other people&apos;s comments and reply
+              to them as well believe that community of learners can help each
+              other grow and improve
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,15 @@
+import CallToAction from "../components/CallToAction";
+
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className="mx-auto flex min-h-screen max-w-2xl flex-col items-center justify-center gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, and JavaScript
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
